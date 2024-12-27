784c1649072d5b6e46e609c1be7dae68f1206540
linhgrr <163371214+linhgrr@users.noreply.github.com>
2024-12-28 01:05:52 +0700

    Time: 5 ms (16.52%) | Memory: 11.3 MB (29.54%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int n;
+    int visited[29];
+    vector<int> curr;
+    vector<int> cur_n;
+    vector<vector<int>> kq;
+
+    bool check(int i, int k){
+        if (k == 0) return true;
+        if (i <= curr[k-1]) return false;
+
+        return !visited[i];
+    }    
+
+    void Try(int k, vector<int>& nums){
+        for (int i = 0; i < n; i++){
+            if (check(i, k)){
+                curr.push_back(i);
+                cur_n.push_back(nums[i]);
+                visited[i] = 1;
+                kq.push_back(cur_n);
+
+                if (k < n) Try(k+1, nums);
+                visited[i] = 0;
+                curr.pop_back();
+                cur_n.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        n = nums.size();
+        sort(nums.begin(), nums.end());
+        kq.push_back(vector<int>());
+        Try(0, nums);
+        
+        set<vector<int>> uniqueSubsets(kq.begin(), kq.end());
+        kq.assign(uniqueSubsets.begin(), uniqueSubsets.end()); 
+        return kq;
+    }
+};
